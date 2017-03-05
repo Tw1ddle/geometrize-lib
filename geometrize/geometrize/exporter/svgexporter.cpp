5b48c6fd28d92426cc156f999ebb28516dd4e30d
sam <sam@silver42.karoo.co.uk>
2017-03-05 19:58:49 +0000

    Minor tweaks to svg export

--- geometrize/geometrize/exporter/svgexporter.cpp
+++ geometrize/geometrize/exporter/svgexporter.cpp
@@ -14,13 +14,25 @@ namespace geometrize
 namespace exporter
 {
 
-std::string exportSVG(const std::vector<geometrize::ShapeResult>& data)
+std::string exportSVG(const std::vector<geometrize::ShapeResult>& data, const std::uint32_t width, const std::uint32_t height)
 {
     std::stringstream stream;
 
+    stream << "<svg xmlns=\"http://www.w3.org/2000/svg\" version=\"1.1\" width=\"" << width << "\" height=\"" << height << "\">" << "\n";
+
+    // TODO width, height
+    // TODO background rect?
+    // TODO transformations (?)
+
     for(const geometrize::ShapeResult& s : data) {
+        // TODO color
+        // TODO opacity
+        // TODO the actual shape data
         stream << s.shape->getSvgShapeData(); // TODO
     }
+
+    stream << "</g>" << "\n";
+    stream << "</svg>" << "\n";
 
     return stream.str();
 }
--- geometrize/geometrize/exporter/svgexporter.h
+++ geometrize/geometrize/exporter/svgexporter.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <cstdint>
+#include <string>
 #include <vector>
 
 namespace geometrize
@@ -16,9 +18,11 @@ namespace exporter
 /**
  * @brief exportSvg Exports shape data to an SVG image.
  * @param data The shape data to export.
+ * @param width The width of the SVG image.
+ * @param height The height of the SVG image.
  * @return A string representing the SVG image.
  */
-std::string exportSvg(const std::vector<geometrize::ShapeResult>& data);
+std::string exportSVG(const std::vector<geometrize::ShapeResult>& data, const std::uint32_t width, const std::uint32_t height);
 
 }
 
