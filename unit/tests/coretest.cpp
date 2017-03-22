98d8a3153baa187adf484f896051a2c27b3cfbc1
sam <sam@silver42.karoo.co.uk>
2017-03-22 23:07:01 +0000

    Get unit tests building again

--- unit/tests/coretest.cpp
+++ unit/tests/coretest.cpp
@@ -12,7 +12,7 @@ geometrize::rgba computeScanlinesColor(const geometrize::rgba targetColor, const
     geometrize::Bitmap current(10, 10, currentColor);
     std::vector<geometrize::Scanline> lines;
     for(int y = 0; y < 10; y++) {
-        lines.push_back(geometrize::Scanline(y, 0, 9, 0));
+        lines.push_back(geometrize::Scanline(y, 0, 9));
     }
     return geometrize::core::computeColor(target, current, lines, alpha);
 }
@@ -29,7 +29,7 @@ TEST_CASE("Test drawing of scanlines", "[core]")
     geometrize::Bitmap image(50, 50, geometrize::rgba{10, 20, 30, 128});
     std::vector<geometrize::Scanline> lines;
     for(int y = 0; y < 50; y++) {
-        lines.push_back(geometrize::Scanline(y, 0, 50, 255));
+        lines.push_back(geometrize::Scanline(y, 0, 50));
     }
 
     // TODO check drawn lines
@@ -45,14 +45,14 @@ TEST_CASE("Test copying of scanlines", "[core]")
     geometrize::Bitmap destination(width, height, geometrize::rgba{0, 0, 0, 0});
 
     std::vector<geometrize::Scanline> scanlines;
-    for(int y = 3; y < 7; y++) {
-        scanlines.push_back(geometrize::Scanline(y, 3, 7, 255));
+    for(std::int32_t y = 3; y < 7; y++) {
+        scanlines.push_back(geometrize::Scanline(y, 3, 7));
     }
 
     geometrize::core::copyLines(destination, source, scanlines);
 
     for(const geometrize::Scanline& line : scanlines) {
-        const std::uint32_t y{line.y};
+        const std::int32_t y{line.y};
         for(std::uint32_t x = line.x1; x < line.x2; x++) {
             REQUIRE(destination.getPixel(x, y) == color);
         }
