7d0437d665fd6b5b237c561f2782765d4cd2ac14
sam <sam@silver42.karoo.co.uk>
2017-05-31 23:02:24 +0100

    Reduce default shape count/mutations

--- geometrize/geometrize/runner/imagerunneroptions.h
+++ geometrize/geometrize/runner/imagerunneroptions.h
@@ -16,8 +16,8 @@ class ImageRunnerOptions
 public:
     geometrize::ShapeTypes shapeTypes = geometrize::ShapeTypes::ELLIPSE; ///< The shape types that the image runner shall use.
     std::uint8_t alpha = 128U; ///< The alpha/opacity of the shapes (0-255).
-    std::uint32_t shapeCount = 200U; ///< The number of candidate shapes that will be tried per model step.
-    std::uint32_t maxShapeMutations = 50U; ///< The number of times each candidate shape will be modified to attempt to find a better fit.
+    std::uint32_t shapeCount = 50U; ///< The number of candidate shapes that will be tried per model step.
+    std::uint32_t maxShapeMutations = 100U; ///< The maximum number of times each candidate shape will be modified to attempt to find a better fit.
     std::uint32_t seed = 0U; ///< The seed for the random number generators used by the image runner.
 };
 
