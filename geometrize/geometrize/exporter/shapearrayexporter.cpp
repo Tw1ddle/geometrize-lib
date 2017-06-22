1c9b7e8efc7fe9a9463242eda0bebdaf96afed37
sam <sam@silver42.karoo.co.uk>
2017-06-22 21:27:44 +0100

    More mingw warning fixes

--- geometrize/geometrize/exporter/shapearrayexporter.cpp
+++ geometrize/geometrize/exporter/shapearrayexporter.cpp
@@ -17,15 +17,15 @@ std::string exportShapeArray(const std::vector<geometrize::ShapeResult>& data)
 {
     std::ostringstream stream;
 
-    for(int i = 0; i < data.size(); i++) {
-        const geometrize::ShapeResult& s{data[i]};
+    for(std::size_t i = 0; i < data.size(); i++) {
+        const geometrize::ShapeResult& s(data[i]);
 
         stream << static_cast<std::underlying_type<geometrize::ShapeTypes>::type>(s.shape->getType()) << "\n";
 
         const std::vector<std::int32_t> shapeData{s.shape->getRawShapeData()};
-        for(int d = 0; d < shapeData.size(); d++) {
+        for(std::size_t d = 0; d < shapeData.size(); d++) {
             stream << shapeData[d];
-            if(d <= shapeData.size() - 2) {
+            if(i != (shapeData.size() - 1)) {
                 stream << ",";
             }
         }
@@ -36,7 +36,7 @@ std::string exportShapeArray(const std::vector<geometrize::ShapeResult>& data)
                << static_cast<std::uint32_t>(s.color.b) << ","
                << static_cast<std::uint32_t>(s.color.a);
 
-        if(i <= data.size() - 2) {
+        if(i != (data.size() - 1)) {
             stream << "\n";
         }
     }
