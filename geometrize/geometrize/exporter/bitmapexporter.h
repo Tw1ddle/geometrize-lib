2546a5b64831080ed28992c1bbc63e9e19bab32a
sam <sam@silver42.karoo.co.uk>
2017-08-15 23:31:25 +0100

    Rename windows bitmap export function

--- geometrize/geometrize/exporter/bitmapexporter.h
+++ geometrize/geometrize/exporter/bitmapexporter.h
@@ -14,11 +14,11 @@ namespace exporter
 {
 
 /**
- * @brief exportBitmap Exports the image data to a RGB888 bitmap image file (BMP).
+ * @brief exportBMP Exports the image data to a RGB888 bitmap image file (BMP).
  * @param bitmapData The image data to save as a bitmap image file.
  * @return A string containing the bitmap data.
  */
-std::string exportBitmap(const geometrize::Bitmap& bitmapData);
+std::string exportBMP(const geometrize::Bitmap& bitmapData);
 
 }
 
